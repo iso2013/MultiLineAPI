e5ab738aed721621901eeedabd067c2d2a406281
iso2013 <blitzcubeowner@gmail.com>
2016-12-19 18:36:26 -0700

    Initial commit (Working)

---
+++ src/main/java/net/blitzcube/score/secondlineapi/manager/SecondLineManager.java
@@ -0,0 +1,137 @@
+package net.blitzcube.score.secondlineapi.manager;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListeningWhitelist;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.PacketListener;
+import com.comphenix.protocol.injector.GamePhase;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Created by iso2013 on 12/19/2016.
+ */
+public class SecondLineManager implements Listener, PacketListener {
+    private static SecondLineManager instance;
+    private Plugin parent;
+    private ProtocolManager protocol;
+    private HashMap<UUID, Stack> stacks;
+    private String defaultMessage;
+
+    public SecondLineManager(Plugin parent) {
+        if (instance != null) throw new IllegalArgumentException("API already initialized!");
+        instance = this;
+
+        this.parent = parent;
+        protocol = ProtocolLibrary.getProtocolManager();
+        protocol.addPacketListener(this);
+
+        stacks = new HashMap<>();
+
+        parent.getServer().getPluginManager().registerEvents(this, parent);
+    }
+
+    public static SecondLineManager getInstance(Plugin parent) {
+        if (instance == null) {
+            new SecondLineManager(parent);
+        }
+        return instance;
+    }
+
+    public void setDefaultMessage(String defaultMessage) {
+        this.defaultMessage = defaultMessage;
+    }
+
+    public void clear(Player p) {
+        stacks.remove(p.getUniqueId()).dispose();
+    }
+
+    public void add(Player p) {
+        if (!stacks.containsKey(p.getUniqueId())) {
+            stacks.put(p.getUniqueId(), new Stack(parent, p, defaultMessage));
+        }
+    }
+
+    public boolean isEnabled(Player p) {
+        return stacks.containsKey(p.getUniqueId());
+    }
+
+    public void setLine(Player p, String line) {
+        add(p);
+        stacks.get(p.getUniqueId()).setLine(line);
+    }
+
+    public void setName(Player p, String tag) {
+        add(p);
+        stacks.get(p.getUniqueId()).setName(tag);
+    }
+
+    @EventHandler
+    public void join(PlayerJoinEvent e) {
+        stacks.put(e.getPlayer().getUniqueId(), new Stack(parent, e.getPlayer(), defaultMessage));
+    }
+
+    @EventHandler
+    public void leave(PlayerQuitEvent e) {
+        stacks.remove(e.getPlayer().getUniqueId()).dispose();
+    }
+
+    @EventHandler
+    public void damage(EntityDamageEvent e) {
+        if (e.getEntity().hasMetadata("STACK_ENTITY")) e.setCancelled(true);
+    }
+
+    public void dispose() {
+        stacks.values().forEach(Stack::dispose);
+        stacks.clear();
+    }
+
+    @Override
+    public void onPacketSending(PacketEvent packetEvent) {
+        PacketContainer packet = packetEvent.getPacket();
+        PacketType type = packetEvent.getPacketType();
+        Stack stack;
+        if ((stack = stacks.get(packetEvent.getPlayer().getUniqueId())) != null) {
+            if (type.equals(PacketType.Play.Server.SPAWN_ENTITY)) {
+                packetEvent.setCancelled(stack.hasEntity(packet.getIntegers().read(0)));
+            } else if (type.equals(PacketType.Play.Server.MOUNT)) {
+                packetEvent.setCancelled(stack.hasEntity(packet.getIntegers().read(0)) || packet.getIntegers().read
+                        (0) == packetEvent.getPlayer().getEntityId());
+            }
+        }
+    }
+
+    @Override
+    public void onPacketReceiving(PacketEvent packetEvent) {}
+
+    @Override
+    public ListeningWhitelist getSendingWhitelist() {
+        return ListeningWhitelist.newBuilder().normal().gamePhase(GamePhase.PLAYING).types(
+                PacketType.Play.Server.MOUNT,
+                PacketType.Play.Server.SPAWN_ENTITY
+        ).build();
+    }
+
+    @Override
+    public ListeningWhitelist getReceivingWhitelist() {
+        return ListeningWhitelist.EMPTY_WHITELIST;
+    }
+
+    @Override
+    public Plugin getPlugin() {
+        return parent;
+    }
+}
---
+++ src/main/java/net/blitzcube/score/secondlineapi/manager/Stack.java
@@ -0,0 +1,143 @@
+package net.blitzcube.score.secondlineapi.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.*;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Created by iso2013 on 12/19/2016.
+ */
+public class Stack {
+    private Plugin parent;
+    private ArrayList<Entity> entities;
+    private ArrayList<Integer> entityIds;
+
+    private ArmorStand firstLine;
+    private ArmorStand secondLine;
+
+    private String line;
+    private String name;
+
+    private Player p;
+
+    public Stack(Plugin parent, Player p, String secondLine){
+        this.parent = parent;
+        entities = new ArrayList<>();
+        entityIds = new ArrayList<>();
+        this.p = p;
+        this.line = secondLine;
+        this.name = p.getName();
+
+        createStack();
+        mountStack();
+        updateLines();
+    }
+
+    public void setLine(String line){
+        this.line = line;
+        updateLines();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        updateLines();
+    }
+
+    public String getLine(){
+        return line;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void dispose(){
+        entities.forEach(Entity::remove);
+    }
+
+    public boolean hasEntity(int id){
+        return entityIds.contains(id);
+    }
+
+    private void updateLines(){
+        if(line == null){
+            firstLine.setCustomName("");
+            secondLine.setCustomName(name);
+
+            firstLine.setCustomNameVisible(false);
+            secondLine.setCustomNameVisible(true);
+        } else {
+            firstLine.setCustomName(name);
+            secondLine.setCustomName(line);
+
+            firstLine.setCustomNameVisible(true);
+            secondLine.setCustomNameVisible(true);
+        }
+    }
+
+    private void createStack(){
+        firstLine = createArmorStand();
+        secondLine = createArmorStand();
+
+        entities.add(createGapSilverfish());
+        entities.add(secondLine);
+        entities.add(createGapSlime());
+        entities.add(createGapSilverfish());
+        entities.add(createGapSilverfish());
+        entities.add(firstLine);
+
+        entityIds.addAll(entities.stream().map(Entity::getEntityId).collect(Collectors.toList()));
+    }
+
+    private void mountStack(){
+        for(int i = 0; i < entities.size(); i++){
+            if(i + 1 < entities.size()){
+                entities.get(i).setPassenger(entities.get(i + 1));
+            }
+        }
+        p.setPassenger(entities.get(0));
+    }
+
+    private Silverfish createGapSilverfish(){
+        Silverfish sf = (Silverfish) p.getWorld().spawnEntity(p.getLocation(), EntityType.SILVERFISH);
+        sf.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 1, true, false));
+        sf.setAI(false);
+        sf.setSilent(true);
+        sf.setGravity(false);
+        sf.setInvulnerable(true);
+        sf.setCanPickupItems(false);
+        sf.setCollidable(false);
+        sf.setMetadata("STACK_ENTITY", new FixedMetadataValue(parent, null));
+        return sf;
+    }
+
+    private Slime createGapSlime(){
+        Slime s = (Slime) p.getWorld().spawnEntity(p.getLocation(), EntityType.SLIME);
+        s.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 1, true, false));
+        s.setAI(false);
+        s.setSilent(true);
+        s.setGravity(false);
+        s.setInvulnerable(true);
+        s.setCanPickupItems(false);
+        s.setCollidable(false);
+        s.setMetadata("STACK_ENTITY", new FixedMetadataValue(parent, null));
+        s.setSize(-1);
+        return s;
+    }
+
+    private ArmorStand createArmorStand(){
+        ArmorStand s = (ArmorStand) p.getWorld().spawnEntity(p.getLocation(), EntityType.ARMOR_STAND);
+        s.setVisible(false);
+        s.setMarker(true);
+        s.setCollidable(false);
+        s.setInvulnerable(true);
+        s.setMetadata("STACK_ENTITY", new FixedMetadataValue(parent, null));
+        return s;
+    }
+}
